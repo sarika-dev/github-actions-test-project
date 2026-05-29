35aa277707c7eaaffeaeb3d1814697f0badbeb84
Sarika-dev <sarikadev89@gmail.com>
2026-05-29 22:41:45 +0530

    updated workflow

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,7 +1,6 @@
-import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import App from "../App";
+import App from "./../App";
 
 test("rendersAppWithoutCrashing", () => {
   render(<App />);
